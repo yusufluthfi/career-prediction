1cf445abca0fb43223bfdad76b0736682d0e036c
Muhammad Yusuf Luthfi Ijlal <130814806+yusufluthfi@users.noreply.github.com>
2025-06-17 22:35:38 +0700

    login.py

--- login.py
+++ login.py
@@ -10,10 +10,10 @@ import time
 def connect_to_db():
     try:
         return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="data_mining_system"
+            host="sql12.freesqldatabase.com",
+            user="sql12785344", 
+            password="3WJcZfxRAQ", 
+            database="sql12785344"
         )
     except mysql.connector.Error as e:
         st.error(f"Gagal terhubung ke database: {e}")
